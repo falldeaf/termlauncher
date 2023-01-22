9c5ed4639de6af37c17d9c52a5dae4e93884b6cc
falldeaf <falldeaf@gmail.com>
2023-01-22 17:39:07 -0500

    trying to package it, plugins now working though?

---
+++ termlaunch/__init__.py
@@ -0,0 +1,13 @@
+"""A TUI keylauncher that uses the Textual library."""
+
+######################################################################
+# Main app information.
+__author__     = "falldeaf"
+__copyright__  = "Copyright 2023, falldeaf"
+__credits__    = [ "falldeaf" ]
+__maintainer__ = "falldeaf"
+__email__      = "falldeaf@gmail.com"
+__version__    = "1.0.0"
+__licence__    = "GPLv3+"
+
+### __init__.py ends here
---
+++ termlaunch/__main__.py
@@ -0,0 +1,12 @@
+"""Main entry point for the application."""
+
+##############################################################################
+# Local imports.
+from .app import run
+
+##############################################################################
+# Main entry point to the app.
+if __name__ == "__main__":
+	run()
+
+### __main__.py ends here
